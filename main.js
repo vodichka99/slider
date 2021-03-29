07e54a336ee37b9e5e5ae16ff68960e0923ab6e2
Eugene <kimevgenijkimpum@gmail.com>
2021-03-29 14:40:59 +0500

    last commit

--- main.js
+++ main.js
@@ -1,100 +1,76 @@
 class Item{
-    constructor(selector){
+    constructor(selector, sliderBlock, next, prev){
         this.selector = document.querySelector(selector)
+        this.sliderBlock = document.querySelector(sliderBlock)
+        this.next = document.querySelector(next)
+        this.prev = document.querySelector(prev)
     }
 }
 class Slider extends Item{
     constructor(options){
-        super(options.selector)
+        super(options.selector, options.sliderBlock, options.next, options.prev)
         
         this.name = options.name
         this.transition = options.transition
         this.direction = options.direction
         this.autoplay = options.autoplay
         this.interval = options.interval
-    }
-}
-class Button extends Item{
-    constructor(options){
-        super(options.selector)
-    }
-}
-class SliderBlock extends Item{
-    constructor(options){
-        super(options.selector)
 
-        this.selector.style.transform = options.transform
-        this.selector.style.transition = options.transition
-        this.selector.style.flexDirection = options.direction
+        this.sliderBlock.style.transform = options.transform
+        this.sliderBlock.style.transition = `${options.transition}s`
+        this.sliderBlock.style.flexDirection = options.direction
+
+        this.t = -200
+        this.tr = 450
+        this.t1 = 90
+        this.tr1 = 230
+
+
+        if(this.autoplay == true){
+            function auto(){
+                slider.nextF()
+                setTimeout(auto, options.interval)
+            }
+            setTimeout(auto, options.interval)
+        }
+
+        
+        this.next.addEventListener('click', function(){
+            slider.nextF()
+        })
+        this.prev.addEventListener('click', function(){
+            slider.prevF()
+        })
     }
     nextF(){
         if(slider.direction == 'row'){
-            t = t - tr
-            this.selector.style.transform = `translate(${t}px, 90px)`
+            this.t = this.t - this.tr
+            this.sliderBlock.style.transform = `translate(${this.t}px, 90px)`
         }else if(slider.direction == 'column'){
-            t1 = t1 - tr1
-            this.selector.style.transform = `translate(-200px, ${t1}px)`
+            this.t1 = this.t1 - this.tr1
+            this.sliderBlock.style.transform = `translate(-200px, ${this.t1}px)`
         }
     }
     prevF(){
         if(slider.direction == 'row'){
-            t = t + tr
-            this.selector.style.transform = `translate(${t}px, 90px)`
+            this.t = this.t + this.tr
+            this.sliderBlock.style.transform = `translate(${this.t}px, 90px)`
         }else if(slider.direction == 'column'){
-            t1 = t1 + tr1
-            this.selector.style.transform = `translate(-200px, ${t1}px)`
+            this.t1 = this.t1 + this.tr1
+            this.sliderBlock.style.transform = `translate(-200px, ${this.t1}px)`
         }
     }
 }
 
-const tr = 450
-let t = -200
-const tr1 = 230
-let t1 = 90
-
-
-let X = 'row'
-let Y = 'column'
-
-
 
 const slider = new Slider({
     selector: '.slider',
+    sliderBlock: '.slider__block',
+    next: '.slider__button-next',
+    prev: '.slider__button-prev',
     name: 'slider',
     transition: 1,
-    direction: X,
+    direction: 'row',
     autoplay: true,
-    interval: 5000
+    interval: 5000,
 })
-
-
-const sliderBlock = new SliderBlock({
-    selector: '.slider__block',
-    transform: `translate(${t}px, 90px)`,
-    transition: `${slider.transition}s`,
-    direction: slider.direction
-})
-
-
-const next = new Button({
-    selector: '.slider__button-next'
-})
-const prev = new Button({
-    selector: '.slider__button-prev'
-})
-
-
-next.selector.addEventListener('click', function(){
-    sliderBlock.nextF()
-})
-prev.selector.addEventListener('click', function(){
-    sliderBlock.prevF()
-})
-
-if(slider.autoplay == true){
-    function auto(){
-        sliderBlock.nextF()
-        setTimeout(auto, slider.interval)
-    }
-    setTimeout(auto, slider.interval)
-}
